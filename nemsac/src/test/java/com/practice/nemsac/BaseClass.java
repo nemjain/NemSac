1e2d87ed4ce2b041b4f13d25cee8bc2e618c184b
JindalS <JindalS@INGUEUCLP000304.camkmg-bangalore.com>
2020-03-29 16:49:36 +0530

    BaseClass Commit

--- nemsac/src/test/java/com/practice/nemsac/BaseClass.java
+++ nemsac/src/test/java/com/practice/nemsac/BaseClass.java
@@ -1,14 +1,21 @@
 package com.practice.nemsac;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import pageClass.homePage;
+import pageClass.loginPage;
+
 public class BaseClass {
 
 	WebDriver driver;
+	homePage objhomePage;
+	loginPage objloginPage;
 
 	@BeforeTest
 	public void beforeTest() {
@@ -16,19 +23,30 @@ public class BaseClass {
 		String driverPath = "C:\\Users\\jindals\\git\\NemSac\\nemsac\\driver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 
 	}
 
 	@Test
-	public void f() {
+	public void f() throws InterruptedException {
 		String URL = "https://uat:uattap123@wwwuat.4wheelparts.com";
 		driver.get(URL);
+	
+		objhomePage = new homePage(driver);
+		objhomePage.click_on_myAccount();
+		objhomePage.click_on_LogIn();
+				
+		objloginPage = new loginPage(driver);
+		objloginPage.enterUserName();
+		objloginPage.enterPassword();
+		objloginPage.submitSignIn();
 
 	}
 
 	@AfterTest
 	public void afterTest() {
-		driver.quit();
+	//	driver.quit();
 	}
 
 }
---
+++ nemsac/src/test/java/commonFunctions/commonFunctionsForAll.java
@@ -0,0 +1,19 @@
+package commonFunctions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class commonFunctionsForAll {
+
+	/*
+	 * WebDriverWait wait; WebDriver driver;
+	 * 
+	 * // Construct your class here public commonFunctionsForAll(WebDriver driver){
+	 * this.driver = driver; wait = new WebDriverWait(driver,30); }
+	 * 
+	 * public void waitExplicit(locator)) {
+	 * wait.until(ExpectedConditions.elementToBeClickable(locator));
+	 * 
+	 * }
+	 */
+}
